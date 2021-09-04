d3f2677a390bd7b6447646c67447c06719fdf285
Gonzalo Bustos <bustosgonzalom@gmail.com>
2021-09-04 14:53:08 -0300

    recursive brute force

--- src/brute_force.cpp
+++ src/brute_force.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 #include "brute_force.h"
 
 using namespace std;
+int M = 10;
 
 pair<int, int> sigma(vector<pair<int, pair<int, int>>> subconjunto) {
     int acumBeneficio = 0;
     int acumContagio = 0;
+	int currentIdx;
+	if (subconjunto.size() != 0) {
+		currentIdx = subconjunto[0].first;
+	}
     for (int i = 0; i < subconjunto.size(); i++) {
+		if (abs(currentIdx - subconjunto[i].first) == 1) {
+			acumContagio = M + 1; //sobrepasa el limite de contagio
+			break;
+		}
         acumBeneficio = acumBeneficio + subconjunto[i].second.first;
         acumContagio = acumContagio + subconjunto[i].second.second;
+		currentIdx = subconjunto[i].first;
     }
     return make_pair(acumBeneficio, acumContagio);
 }
@@ -34,29 +45,25 @@ vector<vector<pair<int, pair<int, int>>>> parts(vector<pair<int, pair<int, int>>
     return res;
 }
 
-int calculoBeneficioBF(vector<pair<int, pair<int, int>>> locales, int M) {
+void calculoBeneficioRec(int &maxBeneficio, vector<vector<pair<int, pair<int, int>>>> &partes, int &index) {
+	if (index == partes.size()){
+		return;
+	}
+	
+	pair<int, int> sumaSubParte = sigma(partes[index]);
+	if (sumaSubParte.second <= M) {
+		maxBeneficio = sumaSubParte.first;
+	}
+	index++;
+	calculoBeneficioRec(maxBeneficio, partes, index);
+}
+
+int calculoBeneficioBF(vector<pair<int, pair<int, int>>> locales) {
     vector<vector<pair<int, pair<int, int>>>> partes = parts(locales);
     int maxBeneficio = 0;
-
-    for (int i = 0; i < partes.size(); ++i) {
-        bool noHayContiguos = true;
-        for (int j = 1; j < partes[i].size(); j++) {
-            if (partes[i][j].first == partes[i][j-1].first+1)
-            {
-                noHayContiguos = false;
-                break;
-            }
-        }
-        if (!noHayContiguos) {
-            continue;
-        }
-        pair<int, int> sumaSubParte = sigma(partes[i]);
-        if (sumaSubParte.first >= maxBeneficio) {
-        if (sumaSubParte.second <= M) {
-            maxBeneficio = sumaSubParte.first;
-        }
-        }
-    }
+	int index = 0;
+	calculoBeneficioRec(maxBeneficio, partes, index);
 
     return maxBeneficio;
 }
+
