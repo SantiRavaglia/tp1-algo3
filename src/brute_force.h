aa0a8ba8da4e32b907013b13d7402deedd57c19c
Gonzalo Bustos <bustosgonzalom@gmail.com>
2021-09-05 20:48:04 -0300

    working bf algorithm

--- src/brute_force.h
+++ src/brute_force.h
@@ -4,4 +4,4 @@ using namespace std;
 
 int BFRec(vector<pair<int, pair<int, int>>> locales, int M, int index, int sumBeneficio, int sumContagio, vector<int> vecIndex);
 
-int calculoBeneficioBF(vector<pair<int, pair<int, int>>> locales, int M);
+int calculoBeneficioBF(vector<pair<int, pair<int, int>>> locales);
