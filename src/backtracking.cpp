2bd171c1b46c6a2151db7c7bf6321c371c8b2376
GonzaloMBustos <54861063+GonzaloMBustos@users.noreply.github.com>
2021-09-11 18:00:46 -0300

    Update backtracking.cpp

--- src/backtracking.cpp
+++ src/backtracking.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int M = 45;
 
 int calculoBeneficioBT(vector<int>& b, vector<int>& c, int index, int bSum, int cSum) {
+    // chequeo de final de llamados recursivos
     if (index >= b.size()) {
         return bSum;
     }
@@ -16,38 +17,9 @@ int calculoBeneficioBT(vector<int>& b, vector<int>& c, int index, int bSum, int 
         return 0;
     }
 
-    // hila fino en si es poda o no
+    // llamados recursivos
     int skipCurrent = calculoBeneficioBT(b, c, index+1, bSum, cSum);
     int countCurrent = calculoBeneficioBT(b, c, index+2, bSum+b[index], cSum+c[index]);
 
     return skipCurrent > countCurrent ? skipCurrent : countCurrent;
 }
-
-// int calculoBeneficioBT(vector<int>& b, vector<int>& c, int index, int bSum, int cSum, int prevIdx) {
-//     // cout << bSum << endl;
-//     if (index >= b.size()) {
-//         return bSum;
-//     }
-
-//     if (cSum + c[index] > M) { // poda por factibilidad, porque se que no existe una solucion factible a partir de este punto
-//         return 0;
-//     }
-
-//     int skipCurrent, countCurrent;
-//     if (index == 0) {
-//         skipCurrent = calculoBeneficioBT(b, c, index+1, bSum, cSum, prevIdx);
-//         countCurrent = calculoBeneficioBT(b, c, index+2, bSum+b[index], cSum+c[index], prevIdx);
-//     } else if (index == 1) {        
-//         skipCurrent = calculoBeneficioBT(b, c, index+1, bSum, cSum, prevIdx);
-//         countCurrent = calculoBeneficioBT(b, c, index+1, bSum+b[index], cSum+c[index], index);
-//     } else {
-//         if (index - prevIdx == 1) {
-//             cout << bSum << endl;
-//             return 0;
-//         }
-//         skipCurrent = calculoBeneficioBT(b, c, index+1, bSum, cSum, prevIdx);
-//         countCurrent = calculoBeneficioBT(b, c, index+1, bSum+b[index], cSum+c[index], index);
-//     }
-
-//     return skipCurrent > countCurrent ? skipCurrent : countCurrent;
-// }
