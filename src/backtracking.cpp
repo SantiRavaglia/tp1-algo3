df0a2e1cd41a0fec9999421d45b2f190004ab0ba
GonzaloMBustos <54861063+GonzaloMBustos@users.noreply.github.com>
2021-09-11 19:22:56 -0300

    Update backtracking.cpp

--- src/backtracking.cpp
+++ src/backtracking.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 int M = 45;
 
-int calculoBeneficioBT(vector<int>& b, vector<int>& c, int index, int bSum, int cSum) {
+int calculoBeneficioBT(vector<int>& b, vector<int>& c, int index, int bSum, int cSum, int cotaOptB) {
     // chequeo de final de llamados recursivos
     if (index >= b.size()) {
         return bSum;
@@ -15,6 +15,10 @@ int calculoBeneficioBT(vector<int>& b, vector<int>& c, int index, int bSum, int 
 
     if (cSum + c[index] > M) { // poda por factibilidad, porque se que no existe una solucion factible a partir de este punto
         return 0;
+    }
+    
+    if (bSum == cotaOptB) { // poda por optimalidad, no existe una solucion mejor que mejor cota de beneficios
+        return bSum;
     }
 
     // llamados recursivos
