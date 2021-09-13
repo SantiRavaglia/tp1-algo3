5a3dada996654715f0d8969a923660b5a14b1d87
SantiRavaglia <santi23062000@gmail.com>
2021-09-12 21:50:30 -0300

    finalizado

--- src/generador-random.py
+++
@@ -1,9 +0,0 @@
-import random
-
-def generar_row():
-    return f'{random.randint(1, 100)} {random.randint(1, 100)}'
-
-for i in range(42):
-    print(generar_row())
-
-# python generador-random.py >> random-numbers.txt
